ae495619cfd55b285c4fabca1bb3d0e99a418fcc
Reza Ahmadi <r.ahmadi@rima.tax>
2025-11-26 09:22:33 +0330

    🌱 init domain layer.

---
+++ src/Libraries/Domain/Entities/Catalogs/CategoryEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Entities.Catalogs;
+
+public sealed class CategoryEntity : BaseEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; private set; } = string.Empty;
+    public string? Description { get; set; }
+
+    #region Relations
+
+    #endregion
+
+    #region Methods
+    public static CategoryEntity Create(string name, string? description)
+    {
+        var category = new CategoryEntity()
+        {
+            Name = name,
+            Description = description
+        };
+
+        return category;
+    }
+    #endregion
+}
---
+++ src/Libraries/Domain/Entities/Catalogs/PlantEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Entities.Catalogs;
+
+public sealed class PlantEntity : BaseEntity
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; private set; } = string.Empty;
+    public string? Description { get; set; }
+
+    #region Relations
+
+    #endregion
+
+    #region Methods
+    public static PlantEntity Create(string name, string? description)
+    {
+        var plant = new PlantEntity()
+        {
+            Name = name,
+            Description = description
+        };
+
+        return plant;
+    }
+    #endregion
+}
